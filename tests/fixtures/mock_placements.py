6b41c959361bd1de99756857cef6b870296cca1a
Mathijs Miermans <m.miermans@gmail.com>
2020-02-06 06:16:39 -0800

    Added sponsored collection app tests

--- tests/fixtures/mock_placements.py
+++ tests/fixtures/mock_placements.py
@@ -11,3 +11,25 @@ mock_placements = [
     }
 
 ]
+
+mock_collection_placements = [
+    {
+        "name": "sponsored-collection",
+        "ad_types": [
+            2401,
+            3617
+        ],
+        "zone_ids": [
+            217692
+        ],
+        "count": 20
+    },
+    {
+        "name": "spocs",
+        "ad_types": [
+            2401,
+            3617
+        ],
+        "count": 20
+    }
+]
