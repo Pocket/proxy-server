fa9de52391c022f43e53ce72f13ef849a51f367d
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2022-12-13 11:37:41 +1100

    fix(geolocation): [BACK-1696] Update GeoIP2 database
    - We are moving from GeoIP2-City to GeoLite2-City database. Updated
    the name of the file the code is expecting to find in the S3 bucket
    (bucket has had new file uploaded already), updated README for local
    installation instructions.

--- app/conf/geolocation.py
+++ app/conf/geolocation.py
@@ -2,5 +2,5 @@ import os
 
 production = staging = development = test = {
     's3_bucket': os.environ.get('GEOIP_S3_BUCKET', 'GEOIP_S3_BUCKET'),
-    's3_key': 'GeoIP2-City.mmdb',
+    's3_key': 'GeoLite2-City.mmdb',
 }
