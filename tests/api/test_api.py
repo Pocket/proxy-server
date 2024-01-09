d5e7620c4a4bc7051bc8145949dd256b502386e9
Daniel Mueller <dmueller@mozilla.com>
2024-01-09 12:40:41 -0500

    adding test cases for version=1 spocs requests

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -5,7 +5,7 @@ import schemathesis
 from aioresponses import aioresponses
 
 from tests.fixtures.mock_factory import get_mocked_geolocation_factory
-from tests.fixtures.mock_decision import mock_decision_2
+from tests.fixtures.mock_decision import mock_decision_2, mock_collection_response
 
 __FACTORY = get_mocked_geolocation_factory()
 with patch("app.geolocation.factory.Factory.get_instance", return_value=__FACTORY):
@@ -23,15 +23,36 @@ MOCK_DECISIONS_RESPONSE = {
     }
 }
 
+MOCK_DECISIONS_COLLECTION_RESPONSE = {
+    "user": {
+        "key": "{12345678-8901-2345-aaaa-bbbbbbcccccc}",
+    },
+    "decisions": {
+        "spocs": [mock_collection_response]
+    }
+}
+
+
 @responses.activate
-@schema.parametrize()
-def test_api(case: schemathesis.Case) -> None:
+@schema.parametrize(endpoint="/user")
+def test_delete_api(case: schemathesis.Case) -> None:
     # Mock call to forget API
     responses.delete("https://e-10250.adzerk.net/udb/10250/")
 
+    # Call Pocket Proxy and validate response
+    response = case.call_asgi()
+    case.validate_response(response)
+
+
+@responses.activate
+@schema.parametrize(endpoint="/spocs")
+def test_spocs_api(case: schemathesis.Case) -> None:
     with aioresponses() as m:
         # Mock call to decisions API
-        m.post("https://e-10250.adzerk.net/api/v2", payload=MOCK_DECISIONS_RESPONSE)
+        if case.body["version"] == 1:
+            m.post("https://e-10250.adzerk.net/api/v2", payload=MOCK_DECISIONS_RESPONSE)
+        else:
+            m.post("https://e-10250.adzerk.net/api/v2", payload=MOCK_DECISIONS_COLLECTION_RESPONSE)
 
         # Call Pocket Proxy and validate response
         response = case.call_asgi()
