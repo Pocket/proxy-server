53808840315d47eab06bf0d8755048b54922eb0b
Marion Lang <mlang@mozilla.com>
2024-01-11 10:40:50 -0800

    Add sentry logging (#92)
    * add sentry integration
    
    * update pipfile.lock
    
    * support different environments
    
    * Update app/provider/sentry_provider.py
    
    Co-authored-by: Can Berk Güder <cbguder@mozilla.com>
    
    ---------
    
    Co-authored-by: Can Berk Güder <cbguder@mozilla.com>

--- app/conf/__init__.py
+++ app/conf/__init__.py
@@ -4,6 +4,7 @@ import logging
 from app.conf import (
     s3_cfg,
     geolocation_cfg,
+    sentry_cfg,
     spocs_cfg,
     adzerk_cfg,
     domain_affinities_cfg,
@@ -15,6 +16,7 @@ logging.info("APP_ENV = " + env)
 
 s3 = getattr(s3_cfg, env)
 geolocation = getattr(geolocation_cfg, env)
+sentry = getattr(sentry_cfg, env)
 spocs = getattr(spocs_cfg, env)
 adzerk = getattr(adzerk_cfg, env)
 domain_affinities = getattr(domain_affinities_cfg, env)
