6c6773afaf30ad6c641aa500ae51d5712be1b479
Daniel Mueller <dmueller@mozilla.com>
2024-02-01 13:36:42 -0500

    filter client disconnect errors from sentry reporting (#100)

--- app/provider/sentry_provider.py
+++ app/provider/sentry_provider.py
@@ -3,6 +3,22 @@ import logging
 import sentry_sdk
 
 from app import conf
+from starlette.requests import ClientDisconnect
+
+
+SENTRY_IGNORE_ERRORS = (
+    ClientDisconnect
+)
+
+
+def before_send(event, hint):
+    if hint and "exc_info" in hint:
+        exc_type, exc_value, tb = hint["exc_info"]
+        if isinstance(exc_value, SENTRY_IGNORE_ERRORS):
+            return None
+
+    return event
+
 
 def sentry_init():
     dsn = conf.sentry['dsn']
@@ -15,5 +31,6 @@ def sentry_init():
             traces_sample_rate=traces_sample_rate,
             profiles_sample_rate=profiles_sample_rate,
             environment=conf.env,
+            before_send=before_send
     )
 
