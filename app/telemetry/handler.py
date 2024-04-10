456f7769cc9dbc0c7c8cbd9ea19167a2da929b49
Daniel Mueller <dmueller@mozilla.com>
2024-04-10 13:02:24 -0400

    support to drop callbacks if they are old for one specific flight, to see if that affects pacing in kevel

--- app/telemetry/handler.py
+++ app/telemetry/handler.py
@@ -3,6 +3,7 @@ import gzip
 import json
 import os
 import urllib.request
+from datetime import datetime
 from urllib.parse import urlencode
 
 NETWORK_ID = os.environ.get("ADZERK_NETWORK_ID", 10250)
@@ -11,10 +12,18 @@ TELEMETRY_PATH_IDS = {
     "/i.gif": "1",
     "/e.gif": "2",
 }
+# Kevel flight id that
+PACING_TEST_FLIGHT_ID = os.environ.get("PACING_TEST_FLIGHT_ID", 5003)
+# 31 minutes in milliseconds; 1 minute of leeway
+# for ads used very near to the 30 minute time and then viewed after
+PACING_TEST_MAX_CALLBACK_DELAY_MILLIS = 31 * 60 * 1000
 
 
 def handle_message(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
+    see gcp-ingestion docs for details on the event structure
+    https://mozilla.github.io/gcp-ingestion/architecture/decoder_service_specification/
+    https://mozilla.github.io/gcp-ingestion/architecture/edge_service_specification/#pubsub-message-schema
 
     :param event: Event payload.
     :param context: Google Cloud Function metadata.
@@ -23,6 +32,7 @@ def handle_message(event, context):
     namespace_key = "document_namespace"
     doctype_key = "document_type"
     user_agent_version_key = "user_agent_version"
+    submission_timestamp_key = "submission_timestamp"
 
     decompressed = gzip.decompress(base64.b64decode(event["data"])).decode("utf-8")
     telemetry = json.loads(decompressed)
@@ -31,43 +41,66 @@ def handle_message(event, context):
     namespace = attributes.get(namespace_key)
     doctype = attributes.get(doctype_key)
     user_agent_version = attributes.get(user_agent_version_key)
+    submission_timestamp = attributes.get(submission_timestamp_key)
 
     if namespace in ["org-mozilla-firefox", "org-mozilla-firefox-beta", "org-mozilla-fenix"]\
             and "spoc" == doctype:  # Android/Glean
         if "metrics" in telemetry:
             text_metrics = telemetry["metrics"].get("text", {})
             if "pocket.spoc_shim" in text_metrics:
-                ping_adzerk(text_metrics["pocket.spoc_shim"])
+                ping_adzerk(text_metrics["pocket.spoc_shim"], submission_timestamp)
     elif "firefox-desktop" == namespace and "spoc" == doctype:  # Desktop/Glean
         if int(user_agent_version) >= 122:
             if "metrics" in telemetry:
                 text_metrics = telemetry["metrics"].get("text", {})
                 if "pocket.shim" in text_metrics:
-                    ping_adzerk(text_metrics["pocket.shim"])
+                    ping_adzerk(text_metrics["pocket.shim"], submission_timestamp)
     elif "activity-stream" == namespace and "impression-stats" == doctype:
         if int(user_agent_version) < 122:  # Desktop/Legacy
             if "tiles" in telemetry:
                 for tile in telemetry["tiles"]:
                     if "shim" in tile:
-                        ping_adzerk(tile["shim"])
+                        ping_adzerk(tile["shim"], submission_timestamp)
 
 
-def ping_adzerk(shim):
+def ping_adzerk(shim, submission_timestamp):
     """Pings AdZerk with a given shim
 
     :param shim: comma-separated shim with an event and checksum component.
     """
     path_id, e, s = shim.split(",")
-    path = get_path(path_id)
-    query = urlencode({"e": e, "s": s})
-    url = "https://e-{network_id}.adzerk.net{path}?{query}".format(
-        network_id=NETWORK_ID, path=path, query=query
-    )
-    with urllib.request.urlopen(url) as response:
-        response.read()
+    if should_ping_adzerk(e, submission_timestamp):
+        path = get_path(path_id)
+        query = urlencode({"e": e, "s": s})
+        url = "https://e-{network_id}.adzerk.net{path}?{query}".format(
+            network_id=NETWORK_ID, path=path, query=query
+        )
+        with urllib.request.urlopen(url) as response:
+            response.read()
 
 
 def get_path(path_id):
     for k, v in TELEMETRY_PATH_IDS.items():
         if v == path_id:
             return k
+
+
+def should_ping_adzerk(shim, submission_timestamp):
+    """
+        base64 decodes the kevel shim and checks the flightID and delay of callback.
+        will skip the Kevel callback for flightIDs being used to test pacing
+    """
+    try:
+        submission_timestamp_millis = int(datetime.fromisoformat(submission_timestamp).timestamp()) * 1000
+        # add back padding characters if needed
+        padded_data = shim + '=' * (-len(shim)%4)
+        kevel_json = json.loads(base64.b64decode(padded_data))
+
+        if kevel_json['fl'] == PACING_TEST_FLIGHT_ID:
+            ad_age_millis = submission_timestamp_millis - kevel_json['ts']
+            if ad_age_millis > PACING_TEST_MAX_CALLBACK_DELAY_MILLIS:
+                return False
+    except:
+        return True
+
+    return True
--- tests/unit/test_telemetry_handler.py
+++ tests/unit/test_telemetry_handler.py
@@ -1,10 +1,18 @@
 import base64
 import gzip
 import json
+import time
 
+from datetime import datetime
 from unittest import TestCase
 from unittest.mock import patch, call
+from urllib.parse import urlencode
 from app.telemetry.handler import handle_message, ping_adzerk
+
+
+def make_encoded_shim(flight_id, timestamp_millis):
+    kevel_shim = f'{{"v":"1.11","av":1963769,"at":3617,"bt":0,"cm":56469793,"ch":36848,"ck":{{}},"cr":340374584,"di":"f9bee25375f147888123a33bfdf871c8","dj":0,"ii":"80740184383e4e5793ec76705421b78a","dm":3,"fc":515667491,"fl":{flight_id},"ip":"34.105.7.247","kw":"us,us-ct","mk":"us","nw":10250,"pc":0.93,"op":0.93,"ec":0,"gm":0,"ep":null,"pr":147518,"rt":2,"rs":500,"sa":"55","sb":"i-0983b4c7d16eabe85","sp":172421,"st":1070098,"uk":"{{4ef12475-fb03-4ac8-881f-8637bfbc76a7}}","zn":217758,"ts":{timestamp_millis},"pn":"spocs","gc":true,"gC":true,"gs":"none","dc":1,"tz":"UTC","ba":1,"fq":1}}'
+    return base64.b64encode(bytes(kevel_shim, 'utf-8')).rstrip(b'=').decode()
 
 
 class TestTelemetryHandler(TestCase):
@@ -13,13 +21,13 @@ class TestTelemetryHandler(TestCase):
         telemetry = {'tiles': [{'shim': '0,foo,bar'}, {'shim': '1,1,2'}, {'shim': '2,a,b'}]}
         data = base64.b64encode(gzip.compress(json.dumps(telemetry).encode('utf-8')))
         attributes = {'document_namespace': 'activity-stream', 'document_type': 'impression-stats',
-                      'user_agent_version': 121}
+                      'user_agent_version': 121, 'submission_timestamp': '2024-03-12T21:02:18.123456Z'}
 
         handle_message(event={'data': data, 'attributes': attributes}, context={})
         mock_ping_adzerk.assert_has_calls([
-            call('0,foo,bar'),
-            call('1,1,2'),
-            call('2,a,b'),
+            call('0,foo,bar', '2024-03-12T21:02:18.123456Z'),
+            call('1,1,2', '2024-03-12T21:02:18.123456Z'),
+            call('2,a,b', '2024-03-12T21:02:18.123456Z'),
         ])
 
     @patch('app.telemetry.handler.ping_adzerk')
@@ -27,27 +35,27 @@ class TestTelemetryHandler(TestCase):
         telemetry = {'metrics': {'text': {'pocket.spoc_shim': '0,foo,bar'}}}
         data = base64.b64encode(gzip.compress(json.dumps(telemetry).encode('utf-8')))
         attributes = {'document_namespace': 'org-mozilla-firefox', 'document_type': 'spoc',
-                      'user_agent_version': 121}
+                      'user_agent_version': 121, 'submission_timestamp': '2024-03-12T21:02:18.123456Z'}
 
         handle_message(event={'data': data, 'attributes': attributes}, context={})
-        mock_ping_adzerk.assert_called_with('0,foo,bar')
+        mock_ping_adzerk.assert_called_with('0,foo,bar', '2024-03-12T21:02:18.123456Z')
 
     @patch('app.telemetry.handler.ping_adzerk')
     def test_handle_message_desktop_spoc_ping(self, mock_ping_adzerk):
         telemetry = {'metrics': {'text': {'pocket.shim': '0,foo,bar'}}}
         data = base64.b64encode(gzip.compress(json.dumps(telemetry).encode('utf-8')))
         attributes = {'document_namespace': 'firefox-desktop', 'document_type': 'spoc',
-                      'user_agent_version': 122}
+                      'user_agent_version': 122, 'submission_timestamp': '2024-03-12T21:02:18.123456Z'}
 
         handle_message(event={'data': data, 'attributes': attributes}, context={})
-        mock_ping_adzerk.assert_called_with('0,foo,bar')
+        mock_ping_adzerk.assert_called_with('0,foo,bar', '2024-03-12T21:02:18.123456Z')
 
     @patch('app.telemetry.handler.ping_adzerk')
     def test_handle_message_desktop_spoc_ping_old_version(self, mock_ping_adzerk):
         telemetry = {'metrics': {'text': {'pocket.shim': '0,foo,bar'}}}
         data = base64.b64encode(gzip.compress(json.dumps(telemetry).encode('utf-8')))
         attributes = {'document_namespace': 'firefox-desktop', 'document_type': 'spoc',
-                      'user_agent_version': 121}
+                      'user_agent_version': 121, 'submission_timestamp': '2024-03-12T21:02:18.123456Z'}
 
         handle_message(event={'data': data, 'attributes': attributes}, context={})
         mock_ping_adzerk.assert_not_called()
@@ -57,7 +65,7 @@ class TestTelemetryHandler(TestCase):
         telemetry = {'metrics': {'text': {'pocket.shim': '0,foo,bar'}}}
         data = base64.b64encode(gzip.compress(json.dumps(telemetry).encode('utf-8')))
         attributes = {'document_namespace': 'firefox-temp', 'document_type': 'spoc',
-                      'user_agent_version': 122}
+                      'user_agent_version': 122, 'submission_timestamp': '2024-03-12T21:02:18.123456Z'}
 
         handle_message(event={'data': data, 'attributes': attributes}, context={})
         mock_ping_adzerk.assert_not_called()
@@ -67,22 +75,54 @@ class TestTelemetryHandler(TestCase):
         telemetry = {'metrics': {'text': {'pocket.shim': '0,foo,bar'}}}
         data = base64.b64encode(gzip.compress(json.dumps(telemetry).encode('utf-8')))
         attributes = {'document_namespace': 'firefox-desktop', 'document_type': 'temp',
-                      'user_agent_version': 122}
+                      'user_agent_version': 122, 'submission_timestamp': '2024-03-12T21:02:18.123456Z'}
 
         handle_message(event={'data': data, 'attributes': attributes}, context={})
         mock_ping_adzerk.assert_not_called()
 
     @patch('urllib.request.urlopen')
     def test_ping_adzerk_r(self, mock_urlopen):
-        ping_adzerk('0,foo,bar')
+        ping_adzerk('0,foo,bar', '2024-03-12T21:02:18.123456Z')
         mock_urlopen.assert_called_once_with("https://e-10250.adzerk.net/r?e=foo&s=bar")
 
     @patch('urllib.request.urlopen')
     def test_ping_adzerk_i(self, mock_urlopen):
-        ping_adzerk('1,foo,bar')
+        ping_adzerk('1,foo,bar', '2024-03-12T21:02:18.123456Z')
         mock_urlopen.assert_called_once_with("https://e-10250.adzerk.net/i.gif?e=foo&s=bar")
 
     @patch('urllib.request.urlopen')
     def test_ping_adzerk_e(self, mock_urlopen):
-        ping_adzerk('2,foo,bar')
+        ping_adzerk('2,foo,bar', '2024-03-12T21:02:18.123456Z')
         mock_urlopen.assert_called_once_with("https://e-10250.adzerk.net/e.gif?e=foo&s=bar")
+
+    @patch('urllib.request.urlopen')
+    def test_ping_adzerk_pacing_test_flight_skipped(self, mock_urlopen):
+        submission_timestamp = '2024-03-12T21:02:18.123456Z'
+        # 35 minutes older than submission
+        submission_timestamp_seconds = int(datetime.fromisoformat(submission_timestamp).timestamp())
+        shim_time = (submission_timestamp_seconds - 35*60) * 1000
+        shim = make_encoded_shim(5003, shim_time)
+        ping_adzerk(f'2,{shim},bar', submission_timestamp)
+        mock_urlopen.assert_not_called()
+
+    @patch('urllib.request.urlopen')
+    def test_ping_adzerk_pacing_non_test_flight_not_skipped(self, mock_urlopen):
+        submission_timestamp = '2024-03-12T21:02:18.123456Z'
+        # 10 minutes older than submission
+        submission_timestamp_seconds = int(datetime.fromisoformat(submission_timestamp).timestamp())
+        shim_time = (submission_timestamp_seconds - 10*60) * 1000
+        shim = make_encoded_shim(5003, shim_time)
+        ping_adzerk(f'2,{shim},bar', submission_timestamp)
+        params = urlencode({'e': shim, 's': 'bar'})
+        mock_urlopen.assert_called_once_with(f"https://e-10250.adzerk.net/e.gif?{params}")
+
+    @patch('urllib.request.urlopen')
+    def test_ping_adzerk_pacing_test_flight_not_skipped(self, mock_urlopen):
+        submission_timestamp = '2024-03-12T21:02:18.123456Z'
+        # 35 minutes older than submission but not a test flight
+        submission_timestamp_seconds = int(datetime.fromisoformat(submission_timestamp).timestamp())
+        shim_time = (submission_timestamp_seconds - 35*60) * 1000
+        shim = make_encoded_shim(504478050, shim_time)
+        ping_adzerk(f'2,{shim},bar', submission_timestamp)
+        params = urlencode({'e': shim, 's': 'bar'})
+        mock_urlopen.assert_called_once_with(f"https://e-10250.adzerk.net/e.gif?{params}")
