43fec102df86406c3c463c902381b80588cbc1c3
Daniel Mueller <dmueller@mozilla.com>
2024-04-29 11:43:49 -0400

    Add sampling support for latency metrics on ad server callbacks (#116)
    * add log based metrics for pipeline & overall latency sampling in cloud function. removed brotlipy because it wouldn't install and isn't ever imported
    
    * add namespace and version to log so that we can differentiate behavior by client versions

--- app/telemetry/handler.py
+++ app/telemetry/handler.py
@@ -1,8 +1,13 @@
 import base64
+import google.cloud.logging
 import gzip
 import json
+import logging
 import os
+import random
+import time
 import urllib.request
+from datetime import datetime, timezone
 from urllib.parse import urlencode
 
 NETWORK_ID = os.environ.get("ADZERK_NETWORK_ID", 10250)
@@ -23,6 +28,7 @@ def handle_message(event, context):
     namespace_key = "document_namespace"
     doctype_key = "document_type"
     user_agent_version_key = "user_agent_version"
+    submission_timestamp_key = "submission_timestamp"
 
     decompressed = gzip.decompress(base64.b64decode(event["data"])).decode("utf-8")
     telemetry = json.loads(decompressed)
@@ -31,6 +37,7 @@ def handle_message(event, context):
     namespace = attributes.get(namespace_key)
     doctype = attributes.get(doctype_key)
     user_agent_version = attributes.get(user_agent_version_key)
+    submission_timestamp = attributes.get(submission_timestamp_key)
 
     if namespace in ["org-mozilla-firefox", "org-mozilla-firefox-beta", "org-mozilla-fenix"]\
             and "spoc" == doctype:  # Android/Glean
@@ -38,18 +45,21 @@ def handle_message(event, context):
             text_metrics = telemetry["metrics"].get("text", {})
             if "pocket.spoc_shim" in text_metrics:
                 ping_adzerk(text_metrics["pocket.spoc_shim"])
+                record_metrics(text_metrics["pocket.spoc_shim"], submission_timestamp, namespace, user_agent_version)
     elif "firefox-desktop" == namespace and "spoc" == doctype:  # Desktop/Glean
         if int(user_agent_version) >= 122:
             if "metrics" in telemetry:
                 text_metrics = telemetry["metrics"].get("text", {})
                 if "pocket.shim" in text_metrics:
                     ping_adzerk(text_metrics["pocket.shim"])
+                    record_metrics(text_metrics["pocket.shim"], submission_timestamp, namespace, user_agent_version)
     elif "activity-stream" == namespace and "impression-stats" == doctype:
         if int(user_agent_version) < 122:  # Desktop/Legacy
             if "tiles" in telemetry:
                 for tile in telemetry["tiles"]:
                     if "shim" in tile:
                         ping_adzerk(tile["shim"])
+                        record_metrics(tile["shim"], submission_timestamp, namespace, user_agent_version)
 
 
 def ping_adzerk(shim):
@@ -71,3 +81,54 @@ def get_path(path_id):
     for k, v in TELEMETRY_PATH_IDS.items():
         if v == path_id:
             return k
+
+
+def record_metrics(shim, submission_timestamp, namespace, user_agent_version):
+    """If METRICS_SAMPLE_RATE is set and greater than 0, then log metrics for the event.
+        The metrics are logged in a structured event to enable google cloud log-based metrics to aggregate them
+
+        metrics sampled:
+            time from adserver generating the impression to now
+            time from telemetry submission to now
+    """
+    try:
+        # range of 0-1000, to sample in increments of 1/1000
+        metrics_sample_rate = os.environ.get("METRICS_SAMPLE_RATE", "0")
+
+        if not metrics_sample_rate.isdecimal():
+            return
+
+        if int(metrics_sample_rate) <= 0 or random.randrange(0, 1000) >= int(metrics_sample_rate):
+            return
+
+        log_client = google.cloud.logging.Client()
+        log_client.setup_logging()
+
+        submission_timestamp = datetime.fromisoformat(submission_timestamp)
+
+        _, encoded_data, _ = shim.split(",")
+        padded_data = encoded_data + '=' * (-len(encoded_data)%4)
+        kevel_json = json.loads(base64.b64decode(padded_data))
+        kevel_timestamp_millis = kevel_json['ts']
+
+        now = get_now()
+
+        glean_latency = now - submission_timestamp
+        glean_latency_millis = int(glean_latency.total_seconds() * 1000)
+        adserver_latency_millis = int(now.timestamp() * 1000) - kevel_timestamp_millis
+
+        json_fields = {
+            "glean_latency": glean_latency_millis,
+            "adserver_latency": adserver_latency_millis,
+            "namespace": namespace,
+            "user_agent_version": user_agent_version,
+        }
+        logging.info("metrics", extra={"json_fields": json_fields})
+
+        return
+    except:
+        return
+
+
+def get_now():
+    return datetime.now(timezone.utc)
--- tests/unit/test_telemetry_handler.py
+++ tests/unit/test_telemetry_handler.py
@@ -1,19 +1,29 @@
 import base64
 import gzip
 import json
+import logging
+import os
+import random
 
-from unittest import TestCase
+from datetime import datetime, timezone
+from unittest import mock, TestCase
 from unittest.mock import patch, call
-from app.telemetry.handler import handle_message, ping_adzerk
+from app.telemetry.handler import handle_message, ping_adzerk, record_metrics
 
+
+def make_encoded_shim(timestamp_millis):
+    kevel_shim = f'{{"v":"1.11","av":1963769,"at":3617,"bt":0,"cm":56469793,"ch":36848,"ck":{{}},"cr":340374584,"di":"f9bee25375f147888123a33bfdf871c8","dj":0,"ii":"80740184383e4e5793ec76705421b78a","dm":3,"fc":515667491,"fl":2000,"ip":"34.105.7.247","kw":"us,us-ct","mk":"us","nw":10250,"pc":0.93,"op":0.93,"ec":0,"gm":0,"ep":null,"pr":147518,"rt":2,"rs":500,"sa":"55","sb":"i-0983b4c7d16eabe85","sp":172421,"st":1070098,"uk":"{{4ef12475-fb03-4ac8-881f-8637bfbc76a7}}","zn":217758,"ts":{timestamp_millis},"pn":"spocs","gc":true,"gC":true,"gs":"none","dc":1,"tz":"UTC","ba":1,"fq":1}}'
+    return base64.b64encode(bytes(kevel_shim, 'utf-8')).rstrip(b'=').decode()
 
 class TestTelemetryHandler(TestCase):
+    @patch('app.telemetry.handler.record_metrics')
     @patch('app.telemetry.handler.ping_adzerk')
-    def test_handle_message_legacy_ping(self, mock_ping_adzerk):
+    def test_handle_message_legacy_ping(self, mock_ping_adzerk, mock_record_metrics):
         telemetry = {'tiles': [{'shim': '0,foo,bar'}, {'shim': '1,1,2'}, {'shim': '2,a,b'}]}
         data = base64.b64encode(gzip.compress(json.dumps(telemetry).encode('utf-8')))
+        submission_timestamp = '2024-04-24T21:02:18.123456Z'
         attributes = {'document_namespace': 'activity-stream', 'document_type': 'impression-stats',
-                      'user_agent_version': 121}
+                      'user_agent_version': 121, 'submission_timestamp': submission_timestamp}
 
         handle_message(event={'data': data, 'attributes': attributes}, context={})
         mock_ping_adzerk.assert_has_calls([
@@ -21,26 +31,37 @@ class TestTelemetryHandler(TestCase):
             call('1,1,2'),
             call('2,a,b'),
         ])
+        mock_record_metrics.assert_has_calls([
+            call('0,foo,bar', submission_timestamp, 'activity-stream', 121),
+            call('1,1,2', submission_timestamp, 'activity-stream', 121),
+            call('2,a,b', submission_timestamp, 'activity-stream', 121),
+        ])
 
+    @patch('app.telemetry.handler.record_metrics')
     @patch('app.telemetry.handler.ping_adzerk')
-    def test_handle_message_android_spoc_ping(self, mock_ping_adzerk):
+    def test_handle_message_android_spoc_ping(self, mock_ping_adzerk, mock_record_metrics):
         telemetry = {'metrics': {'text': {'pocket.spoc_shim': '0,foo,bar'}}}
         data = base64.b64encode(gzip.compress(json.dumps(telemetry).encode('utf-8')))
+        submission_timestamp = '2024-04-24T21:02:18.123456Z'
         attributes = {'document_namespace': 'org-mozilla-firefox', 'document_type': 'spoc',
-                      'user_agent_version': 121}
+                      'user_agent_version': 121, 'submission_timestamp': submission_timestamp}
 
         handle_message(event={'data': data, 'attributes': attributes}, context={})
         mock_ping_adzerk.assert_called_with('0,foo,bar')
+        mock_record_metrics.assert_called_with('0,foo,bar', submission_timestamp, 'org-mozilla-firefox', 121)
 
+    @patch('app.telemetry.handler.record_metrics')
     @patch('app.telemetry.handler.ping_adzerk')
-    def test_handle_message_desktop_spoc_ping(self, mock_ping_adzerk):
+    def test_handle_message_desktop_spoc_ping(self, mock_ping_adzerk, mock_record_metrics):
         telemetry = {'metrics': {'text': {'pocket.shim': '0,foo,bar'}}}
         data = base64.b64encode(gzip.compress(json.dumps(telemetry).encode('utf-8')))
+        submission_timestamp = '2024-04-24T21:02:18.123456Z'
         attributes = {'document_namespace': 'firefox-desktop', 'document_type': 'spoc',
-                      'user_agent_version': 122}
+                      'user_agent_version': 122, 'submission_timestamp': submission_timestamp}
 
         handle_message(event={'data': data, 'attributes': attributes}, context={})
         mock_ping_adzerk.assert_called_with('0,foo,bar')
+        mock_record_metrics.assert_called_with('0,foo,bar', submission_timestamp, 'firefox-desktop', 122)
 
     @patch('app.telemetry.handler.ping_adzerk')
     def test_handle_message_desktop_spoc_ping_old_version(self, mock_ping_adzerk):
@@ -86,3 +107,61 @@ class TestTelemetryHandler(TestCase):
     def test_ping_adzerk_e(self, mock_urlopen):
         ping_adzerk('2,foo,bar')
         mock_urlopen.assert_called_once_with("https://e-10250.adzerk.net/e.gif?e=foo&s=bar")
+
+    @patch('google.cloud.logging')
+    @patch('logging.info')
+    def test_record_metrics_no_sampling(self, mock_logging, mock_google_logging):
+        os.environ["METRICS_SAMPLE_RATE"] = "0"
+        shim = make_encoded_shim(1713971071000)
+        record_metrics(f'2,{shim},bar', '2024-04-24T21:02:18.123456Z', "firefox-desktop", 125)
+        mock_logging.assert_not_called()
+
+    @patch('google.cloud.logging')
+    @patch('logging.info')
+    def test_record_metrics_sampling_misconfigured(self, mock_logging, mock_google_logging):
+        os.environ["METRICS_SAMPLE_RATE"] = "true"
+        shim = make_encoded_shim(1713971071000)
+        record_metrics(f'2,{shim},bar', '2024-04-24T21:02:18.123456Z', "firefox-desktop", 125)
+        mock_logging.assert_not_called()
+
+    @patch('google.cloud.logging')
+    @patch('logging.info')
+    def test_record_metrics_sample_rate_excluded(self, mock_logging, mock_google_logging):
+        # seed random to ensure random sample is excluded
+        random.seed(0)
+        os.environ["METRICS_SAMPLE_RATE"] = "500"
+        shim = make_encoded_shim(1713971071000)
+        record_metrics(f'2,{shim},bar', '2024-04-24T21:02:18.123456Z', "firefox-desktop", 125)
+        mock_logging.assert_not_called()
+
+    @patch('google.cloud.logging')
+    @patch('logging.info')
+    def test_record_metrics_shim_not_parsable(self, mock_logging, mock_google_logging):
+        os.environ["METRICS_SAMPLE_RATE"] = "1000"
+        record_metrics(f'2,shim,bar', '2024-04-24T21:02:18.123456Z', "firefox-desktop", 125)
+        mock_logging.assert_not_called()
+
+    @patch('google.cloud.logging')
+    @patch('logging.info')
+    def test_record_metrics_timestamp_not_parsable(self, mock_logging, mock_google_logging):
+        os.environ["METRICS_SAMPLE_RATE"] = "1000"
+        shim = make_encoded_shim(1713971071000)
+        record_metrics(f'2,{shim},bar', 'invalid-timestamp', "firefox-desktop", 125)
+        mock_logging.assert_not_called()
+
+    @patch('app.telemetry.handler.get_now', mock.MagicMock(return_value=datetime(2024, 4, 25, 15, 4, 44, 686803, tzinfo=timezone.utc)))
+    @patch('google.cloud.logging')
+    @patch('logging.info')
+    def test_log_metrics_sample_rate_included(self, mock_logging, mock_google_logging):
+        # seed random to ensure random sample is included
+        random.seed(0)
+        os.environ["METRICS_SAMPLE_RATE"] = "900"
+        shim = make_encoded_shim(1713971071000)
+        record_metrics(f'2,{shim},bar', '2024-04-24T21:02:18.123456Z', "firefox-desktop", 125)
+        json_fields = {
+            "glean_latency": 64946563,
+            "adserver_latency": 86413686,
+            "namespace": "firefox-desktop",
+            "user_agent_version": 125
+        }
+        mock_logging.assert_called_once_with("metrics", extra={"json_fields": json_fields})
